511157d64ed8568ac91ef10df7f9ca8b306e49bf
asserkhairallah9 <matrixkoon99@gmail.com>
2025-05-09 04:40:09 +0300

    initial

---
+++ Planner.java
@@ -0,0 +1,204 @@
+
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+
+public class Planner extends JPanel implements ActionListener 
+{
+  JPanel FirstPanel ;
+   static JPanel SecondPanel;
+    static int numberOfTasks;
+  JComboBox <String> DayComboBox;
+  static JProgressBar Bar;
+    JScrollPane ScrollPane;
+    JLabel productivityLabel;
+	private JButton SwitchButton3 = new JButton();
+	private JButton PreviousButton3 = new JButton();
+
+
+  //JComboBox = A component that combines a button or editable field and a drop down list.
+   //             remember that we need to pass an array of reference data type.
+  //  JCheckBox = GUI components that can be selected or deslected.
+  
+  public Planner()
+  {
+    //Main panel.
+    this.setLayout(null);
+		this.setBackground(new Color(46,46,46));
+  
+
+        // first Panel for week combo box.    
+    FirstPanel = new JPanel();
+    FirstPanel.setBackground(new Color(0, 0 ,0, 0));
+    FirstPanel.setBounds(50, 40, 300, 200);
+      JLabel Label = new JLabel(); 
+      Label.setText("Select The Day");
+      Label.setForeground(Color.white);
+    Label.setFont(new Font("MV Boli",Font.PLAIN ,35));
+    String[] days = {"Saturday","Sunday","Monday","Tuesday","Wedensday","Thursday","Friday"};
+    DayComboBox = new JComboBox (days);
+    DayComboBox.setSize(50,20);
+    DayComboBox.setFont(new Font("MV Boli",Font.PLAIN ,20));
+    DayComboBox.addActionListener(this);
+    
+    FirstPanel.add(Label);
+    FirstPanel.add(DayComboBox);
+    FirstPanel.setBounds(50,50,350,150);
+    this.add(FirstPanel);
+    
+    //title panel for the second panel.
+    JPanel titlePanel = new JPanel();
+    titlePanel.setBackground(new Color(0, 0 ,0, 0));
+    JLabel labelx = new JLabel("Enter your tasks");
+    labelx.setForeground(Color.white);
+    labelx.setFont(new Font("MV Boli",Font.PLAIN ,20));
+    titlePanel.add(labelx);
+    titlePanel.setBounds(500, 0, 600, 40);
+    this.add(titlePanel);
+    //Second panel.
+    SecondPanel = new JPanel();
+    SecondPanel.setBackground(new Color(0, 0 ,0, 0));
+    SecondPanel.setBounds(500, 90, 600, 600);
+    
+    
+    Bar = new JProgressBar();
+    Bar.setValue(0);
+    Bar.setBounds(50, 250, 400, 50);
+    Bar.setFont(new Font (null,Font.BOLD,25));
+    Bar.setStringPainted(true);
+	
+    this.add(SecondPanel);  
+
+
+	 SwitchButton3.setBounds(1200, 650, 160, 100);
+	 SwitchButton3.setText("Finish");
+	 SwitchButton3.setBackground(Color.WHITE);
+	 SwitchButton3.setFont(new Font("MV Boli",Font.BOLD ,40));
+	 SwitchButton3.setFocusable(false);
+		SwitchButton3.addActionListener(new ActionListener() {
+			@Override public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Start Over");
+				Main.changecard("Welcome Panel");
+				setDefault();
+
+		}});	 
+	PreviousButton3.setBounds(40, 650, 250, 100);
+	 PreviousButton3.setBackground(Color.WHITE);
+	 PreviousButton3.setText("Back");
+	 PreviousButton3.setFont(new Font("MV Boli",Font.BOLD ,40));
+	 PreviousButton3.setFocusable(false);
+		PreviousButton3.addActionListener(new ActionListener() {
+			@Override public void actionPerformed(ActionEvent e) {
+				Main.changecard("Task No.s");
+				setDefault();
+		}});
+
+	this.add(SwitchButton3);
+	this.add(PreviousButton3);
+
+
+    this.add(Bar);
+  }
+
+  	public static void GetDefault(){
+		numberOfTasks = TaskNumberPanel.getTaskCount();
+		SecondPanel.setLayout(new GridLayout(numberOfTasks, numberOfTasks, 10, 10));
+    for (int i = 0; i < numberOfTasks; i++)
+    {
+       JTextField taskField = new JTextField("Task " + (i + 1));
+       taskField.setFont(new Font("MV Boli",Font.PLAIN ,20));
+       JCheckBox taskCheck = new JCheckBox("Done");
+       taskCheck.setFont(new Font("MV Boli",Font.PLAIN ,20));
+		      Bar.setString(0 + " / " + numberOfTasks);
+			        Bar.setMaximum(numberOfTasks);
+      Bar.setValue(0);
+
+	   taskCheck.addItemListener(e-> updateProgressBar());
+        SecondPanel.add(taskField);
+        SecondPanel.add(taskCheck);
+    }
+	}
+
+	public void setDefault(){
+		SecondPanel.removeAll();
+		this.setBackground(new Color(46,46,46));
+	}
+  
+  private static void updateProgressBar()   /// update
+  {
+	 int Done = 0;
+    for(Component c:SecondPanel.getComponents())
+    {
+
+      if(c instanceof JCheckBox)
+      {
+        
+      if (((JCheckBox) c).isSelected()) 
+          {
+           Done++;
+        }}
+        
+      Bar.setValue(Done);
+      Bar.setString(Done + " / " + numberOfTasks);
+    }
+  
+  }
+  
+  
+  @Override
+  public void actionPerformed(ActionEvent e)// Response for events.
+  {
+   if(e.getSource()==DayComboBox)
+   {
+     String SelectedDay = (String) DayComboBox.getSelectedItem();
+     
+     if(SelectedDay == "Saturday" )
+     {
+       this.setBackground(new Color(46,46,46));
+     }
+     else if(SelectedDay == "Sunday" )
+     {
+       this.setBackground(new Color(44,120,108));
+     }
+     else if(SelectedDay == "Monday" )
+     {
+       this.setBackground(new Color(53,80,112));
+     }
+     else if(SelectedDay == "Tuesday" )
+     {
+       this.setBackground( new Color(139,69,19));
+     }
+     else if(SelectedDay == "Wedensday" )
+     {
+       this.setBackground(new Color (128,0,32));
+     }
+     else if(SelectedDay == "Thursday" )
+     {
+       this.setBackground(new Color(188,143,143));
+     }
+     else if(SelectedDay == "Friday" )
+     {
+       this.setBackground( new Color(75,56,50));
+     }
+     
+     
+     
+   }
+    
+  }
+
+}
